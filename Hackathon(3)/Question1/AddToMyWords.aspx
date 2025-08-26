5094f285dee3e2b84a41a8b03af05e807849d0bb
Ashirwad8981 <ashirwad8981@gmail.com>
2025-08-26 12:17:36 +0530

    Add files via upload

---
+++ Hackathon(3)/Question1/AddToMyWords.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="AddToMyWords.aspx.cs" Inherits="SampleWebFormsApp.AddToMyWords" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            margin-top: 0px;
+        }
+        .auto-style2 {
+            margin-left: 64px;
+            margin-top: 0px;
+        }
+        .auto-style3 {
+            margin-left: 56px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <h2>Words&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Translation&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Action</h2>
+        <asp:Label ID="lbl1" runat="server" CssClass="auto-style1" Height="187px" Width="104px"></asp:Label>
+        <asp:Label ID="lbl2" runat="server" CssClass="auto-style2" Width="519px"></asp:Label>
+        <asp:Button ID="txtButton2" runat="server" CssClass="auto-style3" Text="Action" Width="216px" />
+    </form>
+</body>
+</html>
---
+++ Hackathon(3)/Question1/TranslationDisplay.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SampleWebFormsApp
+{
+    public partial class TranslationDisplay : System.Web.UI.Page
+    {
+        private static Dictionary<string, string> dictionary = new Dictionary<string, string>();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // private Dictionary<string, string> translations;
+            //private static Dictionary<string, string> dictionary = new Dictionary<string, string>();
+
+            //string word = txtBox1.Text.Trim().ToLower();
+
+            //if (TranslationStore.Dictionary.ContainsKey(word))
+            //{
+            //    // Store word in session and redirect
+            //    Session["Word"] = word;
+            //    Session["Translation"] = TranslationStore.Dictionary[word];
+            //    Response.Redirect("Result.aspx");
+            //}
+            //else
+            //{
+            //    lblMessage.Text = "Translation not found.";
+            //}
+
+
+            if (!IsPostBack && Request.QueryString["message"] != null)
+            {
+                lblMessage.Text = Request.QueryString["message"];
+            }
+
+        }
+
+        protected void txtButton_Click(object sender, EventArgs e)
+        {
+            string word = txtBox1.Text.Trim().ToLower();
+
+            if (TranslationStore.Dictionary.ContainsKey(word))
+            {
+                // Store word in session and redirect
+                Session["Word"] = word;
+                Session["Translation"] = TranslationStore.Dictionary[word];
+                Response.Redirect("AddToMyWords.aspx");
+            }
+            else {
+
+                {
+                    // Redirect back with message
+                    lblMessage.Text = "Translation not found";
+                }
+            }
+        }
+    }
+}
